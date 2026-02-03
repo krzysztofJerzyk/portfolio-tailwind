077016dcd2cae036177153611a17633a533a7202
Krzysztof Jerzyk <jerzyk.krzysztof.1993@gmail.com>
2026-02-03 14:48:36 +0100

    Add image optimization

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ fetch(`https://api.github.com/users/${userName}/repos?direction=${direction}`)
       }
 
       const element = `<article
-            class="bg-linear-to-br from-white/10 to-white/5 rounded-10x md:rounded-10xl overflow-clip"
+            class="bg-linear-to-br from-white/10 to-white/5 rounded-10x md:rounded-10xl overflow-clip flex flex-col h-full"
           >
             <div
               class="h-11 bg-linear-to-br from-white/10 to-white/5 p-4 flex gap-1.5 shadow-innerLight rounded-t-10x md:rounded-t-10xl border-b border-bg"
@@ -36,7 +36,8 @@ fetch(`https://api.github.com/users/${userName}/repos?direction=${direction}`)
               <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
               <span class="w-3 h-3 block rounded-full bg-bg opacity-50"></span>
             </div>
-            <div class="p-5 md:p-6 lg:p-10">
+            <div class="p-5 md:p-6 lg:p-10 flex flex-col justify-between grow">
+            <div>
               <header class="flex gap-4 items-center mb-4">
                 <h3 class="text-2xl font-bold leading-none">${name}</h3>
                 <p
@@ -48,12 +49,13 @@ fetch(`https://api.github.com/users/${userName}/repos?direction=${direction}`)
               <p class="text-gray-400 text-xl mb-4">
                 ${description}
               </p>
-              <ul class="flex gap-2 mb-10">
+              <ul class="flex gap-2 mb-10 flex-wrap">
                 ${tags}
               </ul>
-              <div class="flex flex-col md:flex-row items-start gap-4">
+              </div>
+              <div class="flex flex-col flex-wrap md:flex-row items-start gap-4">
                 <a
-                  class="bg-bg text-accent flex gap-3 font-bold py-4 px-5 items-center rounded-10x md:rounded-xl md:text-xl border-lightGray border-2"
+                  class="bg-bg text-accent flex gap-3 font-bold py-4 px-5 items-center rounded-10x md:rounded-xl md:text-xl border-lightGray border-2 hover:border-accent transition-colors duration-500"
                   href="${homepage}"
                   target="_blank"
                   rel="noreferrer notfollow"
@@ -61,7 +63,7 @@ fetch(`https://api.github.com/users/${userName}/repos?direction=${direction}`)
                   demo</a
                 >
                 <a
-                  class="bg-bg text-accent flex gap-3 font-bold py-4 px-5 items-center rounded-10x md:rounded-xl md:text-xl border-lightGray border-2"
+                  class="bg-bg text-accent flex gap-3 font-bold py-4 px-5 items-center rounded-10x md:rounded-xl md:text-xl border-lightGray border-2 hover:border-accent transition-colors duration-500"
                   href="${html_url}"
                     target="_blank"
                   rel="noreferrer notfollow"
